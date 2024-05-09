66e34916d66d4ff1b10f84d4283d7a7403c3f782
akashsingh_bhandari <asbhandari14@gmail.com>
2024-05-09 18:40:11 +0530

    Fixing some other error

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -4,7 +4,7 @@ const FAQ = () => {
   return (
     <div className='section w-full h-auto flex justify-center items-center my-12'>
       <div className="container w-[90%] h-auto flex flex-col items-start justify-start">
-        <div className='w-[97px] font-[Inter] font-[700] text-[48px] leading-[58.09px] tracking-[0.5%] text-transparent  bg-clip-text bg-gradient-to-b from-[#06286E] to-[#164EC0]'>FAQ</div>
+        <div className='w-[97px] h-[35px] flex justify-center items-center font-[Inter] font-[700] text-[48px] leading-[58.09px] tracking-[0.5%] text-transparent bg-clip-text bg-gradient-to-b from-[#06286E] to-[#164EC0]'>FAQ</div>
 
         <div className="accordion_box w-[848px] h-[238px] flex flex-col gap-[32px] my-12">
           <div className='w-full h-[58px] rounded-[12px] border-[1px] border-[#164EC0] flex justify-start items-center px-[24px] text-black'>Can education flashcards be used for all age groups?</div>
--- src/Components/PublishedBy.jsx
+++ src/Components/PublishedBy.jsx
@@ -10,13 +10,13 @@ const PublishedBy = () => {
                 <img src="../Images/Logo.png" alt="" />
               </div>
               <div className="w-[115.14px] h-[60.17px] flex flex-col justify-start items-center">
-                <p className='w-full'>Published By</p>
+                <p className='w-full text-[#696671]'>Published By</p>
                 <img src="../Images/HygexxLogo.png" alt="" />
               </div>
             </div>
 
             <div className="createFlashCard_box w-[299px] h-[60px] flex justify-between items-center gap-[8px]">
-              <button className='w-[50px] h-[50px] rounded-[50%] bg-gradient-to-b from-[#06286E] to-[#164EC0] text-5xl text-white'>+</button>
+              <button className='w-[50px] h-[50px] rounded-[50%] bg-gradient-to-b from-[#06286E] to-[#164EC0] text-4xl text-white'>+</button>
               <p className='w-full h-full flex justify-center items-center text-transparent  bg-clip-text bg-gradient-to-b from-[#06286E] to-[#164EC0] font-[Inter] leading-[33.89px] font-[600] text-[28px]'>Create Flashcard</p>
             </div>
         </div>
